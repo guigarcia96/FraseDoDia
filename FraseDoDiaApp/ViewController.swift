e70621f47d771ddddf7b64e62159d3369cd7bb8c
Guilherme <gmoraes1@outlook.com>
2020-11-08 09:23:10 -0800

    Initial Commit

---
+++ FraseDoDiaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FraseDoDiaApp
+//
+//  Created by user183869 on 11/8/20.
+//  Copyright © 2020 GuilhermeMoraes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
