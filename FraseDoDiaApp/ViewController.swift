a9e6b4b7854ef96c8349a91a8856ba1c65f6b76a
Guilherme <gmoraes1@outlook.com>
2020-11-08 10:43:39 -0800

    First Commit

--- FraseDoDiaApp/ViewController.swift
+++ FraseDoDiaApp/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var btnDescobrir: UIButton!
+    
+    @IBOutlet var txtFraseDoDia: UITextView!
+    
+    
+    var frasesDoDia = ["Enfrente os problemas e leve a melhor!", "Dê mais atenção ao que você tem de bom na sua vida", "Para chegar ao topo, o que importa é começar!", "De nada adianta ter sonhos, se você não se empenhar em correr atrás", "Positividade para começar o dia é colocar a sua fé em prática"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        	
     }
 
-
+    @IBAction func mostraFrase(_ sender: Any) {
+        let random = Int.random(in: 0..<frasesDoDia.count)
+        txtFraseDoDia.text = frasesDoDia[random]
+        
+    }
+    
 }
 
